e2b73c807ea606abb6f68f0dced2517e283af9a0
Amit Joshi <ajoshi@netflix.com>
2018-01-07 22:20:58 -0800

    hw0 done

--- hw0/wc.c
+++ hw0/wc.c
@@ -11,6 +11,49 @@ typedef struct {
     size_t size;
 } alloc_info;
 
+typedef struct {
+    int num_lines;
+    int num_words;
+    int num_characters;
+} wc_info;
+
+wc_info scan_buffer(char *buf, int length) {
+    int num_lines = 0;
+    int num_words = 0;
+    int num_characters = 0;
+    int i;
+    short inside_space;
+    short inside_characters;
+    wc_info result;
+
+    for (i = 0; i < length; i++) {
+        if(buf[i] == '\n') {
+            num_lines++;
+            if (inside_characters) {
+                num_characters++;
+                inside_characters = 0;
+                num_words++;
+            }
+        } else if (buf[i] == ' ') {
+            num_characters++;
+            inside_characters = 0;
+            if (!inside_space) {
+               num_words++;
+               inside_space = 1;
+            }
+        } else if (buf[i] != ' ' || buf[i] != '\n') {
+            inside_space = 0;
+            inside_characters = 1;
+            num_characters++;
+        }
+    }
+
+    result.num_lines = num_lines;
+    result.num_characters = num_characters;
+    result.num_words = num_words;
+
+    return result;
+}
 
 alloc_info* expand_buffer(alloc_info* buf) {
     int new_buf_length = buf->size + page_size;
@@ -65,30 +108,23 @@ char * read_from_file(char * file_name) {
 int main(int argc, char *argv[]) {
     int i;
     char *buf;
+    char * fname = "";
+    wc_info scan_result;
 
-    printf("Welcome to line/word/character counting program\n");
-    printf("Lets start with basics - argc = %d\n", argc);
-
-    for (i=0; i < argc; i++) {
-        printf("ARGV[%d] = %s\n", i, argv[i]);
-    }
-    
     if (argc == 1) {
         // read from stdin
         buf = read_stdin();
-
     } else if (argc == 2) {
         // read from file
         buf = read_from_file(argv[1]);
+        fname = argv[1];
     } else {
         fprintf(stderr, "Invalid arguments.");
         exit(-1);
     }
 
+    scan_result = scan_buffer(buf, strlen(buf));
 
-    printf("Stream Contents read \n");
-    printf("%s\n", buf);
-
-    printf("THE END\n");
+    printf("%d\t%d\t%d\t%s\n", scan_result.num_lines, scan_result.num_words, scan_result.num_characters, fname);
     return 0;
 }
